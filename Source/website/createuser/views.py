967274c1d162103a84056703149900f8cac1559c
SolsticeDante <solsticedante@gmail.com>
2019-03-12 16:45:50 +0800

    removed ID from createuser, smoothened process of creating user

--- Source/website/createuser/views.py
+++ Source/website/createuser/views.py
@@ -1,36 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 from .forms import UserForm
 
 def get_user(request):
+	if request.method == 'POST':
+		form = UserForm(request.POST)
+		if form.is_valid():
+			username = request.POST['username']
+			password = request.POST['password']
+			email = request.POST['email']
+			phoneNumber = request.POST['phoneNumber']
 
-	if (request.user.is_authenticated):
-		# user is logged in
-		if (request.user.is_superuser):
-			# user is superuser
-			if request.method == 'POST':
-				form = UserForm(request.POST)
-				if form.is_valid():
-					username = request.POST['username']
-					password = request.POST['password']
-					id = request.POST['id']
-					email = request.POST['email']
-					phoneNumber = request.POST['phoneNumber']
+			user = User.objects.create_user(username, email, password)
+			user.save()
 
-					user = User.objects.create_user(username, email, password)
-					user.save()
+			return HttpResponseRedirect(reverse('login:index'))
 
-					return HttpResponseRedirect('login:index')
-
-			# if a GET (or any other method) we'll create a blank form
-			else:
-				form = UserForm()
-
-			return render(request, 'createuser/user.html', {'form': form})
-		else:
-			# user is normal user
-			return HttpResponseForbidden()
+	# if a GET (or any other method) we'll create a blank form
 	else:
-		return HttpResponseRedirect('login:index')
+		form = UserForm()
+
+	return render(request, 'createuser/user.html', {'form': form})
+
