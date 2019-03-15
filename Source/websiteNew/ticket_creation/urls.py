0aa12749e1ca05ffc3eef8b959fe6f12b4df84af
Liu Zhaohong <liuzh@LiudeMacBook-Pro.local>
2019-03-15 12:36:14 +0800

    updatee

---
+++ Source/websiteNew/ticket_creation/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'ticket_creation'
+urlpatterns = [
+    path('', views.create, name='create'),
+    path('display/', views.list, name='display'),
+    path('delete/', views.delete, name='delete'),
+    path('detail/', views.detail, name='detail'),
+    path('resolve/', views.resolve, name='resolve'),
+]
