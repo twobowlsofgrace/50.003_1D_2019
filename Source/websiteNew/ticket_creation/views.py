cfcaedcab60d50ef18b9054630831d12af94821b
SolsticeDante <solsticedante@gmail.com>
2019-03-22 18:33:16 +0800

    halfway through checking input validity for ticket creation

--- Source/websiteNew/ticket_creation/views.py
+++ Source/websiteNew/ticket_creation/views.py
@@ -8,6 +8,8 @@ from django.contrib import messages
 
 from input_field_test import Input_field_test
 
+# code not verified
+
 
 error_message = None
 error_message_forbidden_administrator = "This feature is not available to administrators"
@@ -26,16 +28,73 @@ def create(request):
 				title_validity = []
 				email_validity = []
 				description_validity = []
+				id_validity = []
+				username = None
+				title = None
+				email = None
+				description = None
+				id = None
 
-				id = 5
-				username = request.POST.get("username")
-				title = request.POST.get("title")
-				email = request.POST.get('email')
-				description = request.POST.get('description')
-				ticket = models.Ticket(ticket_id=id, title=title, resolved=0, read=0, description=description, user=username)
-				ticket.save()
-				messages.add_message(request, messages.SUCCESS, 'Create Successful')
+				try:
+					id = 5
+					username = request.POST.get("username")
+					title = request.POST.get("title")
+					email = request.POST.get('email')
+					description = request.POST.get('description')
+				except ValueError:
+					pass
+
+				# testing input field validity
+				input_field_test = Input_field_test()
+				username_validity = input_field_test.username(username)
+				title_validity = input_field_test.ticket_title(title)
+				email_validity = input_field_test.email(email)
+				description_validity = input_field_test.ticket_description(description)
+				id_validity = input_field_test.ticket_id(id)
+
+				if len(username_validity)==1 and len(title_validity)==1 and len(email_validity)==1 and len(description_validity)==1 and len(id_validity)==1:
+					# input fields are valid
+					ticket = models.Ticket(ticket_id=id, title=title, resolved=0, read=0, description=description, user=username)
+					ticket.save()
+					messages.add_message(request, messages.SUCCESS, 'Create Successful')
+					return render(request, 'ticketcreation/creation.html', {'error_message':error_message})
+				else:
+					# input fields are not valid
+					for i in username_validity:
+		                                if i == "empty":
+		                                        empty_input_state = True
+		                                elif i == "invalid value":
+		                                        invalid_input_state = True
+		                        for i in title_validity:
+		                                if i == "empty":
+		                                        empty_input_state = True
+		                                elif i == "invalid value":
+		                                        invalid_input_state = True
+		                        for i in email_validity:
+		                                if i == "empty":
+		                                        empty_input_state = True
+		                                elif i == "invalid value":
+		                                        invalid_input_state = True
+		                        for i in description_validity:
+		                                if i == "empty":
+		                                        empty_input_state = True
+		                                elif i == "invalid value":
+		                                        invalid_input_state = True
+		                        for i in id_validity:
+		                                if i == "empty":
+		                                        empty_input_state = True
+		                                elif i == "invalid value":
+		                                        invalid_input_state = True
+
+		                        if empty_input_state:
+		                                # input fields are empty
+		                                error_message = error_message_empty_input
+		                        elif invalid_input_state:
+		                                # input fields have invalid input
+		                                error_message = error_message_invalid_input
+
 				return render(request, 'ticketcreation/creation.html', {'error_message':error_message})
+
 		else:
 			# user is superuser
 			return HttpResponseForbidden(error_message_forbidden_administrator)
