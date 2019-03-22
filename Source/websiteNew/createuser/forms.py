79a9ef1bf9771b6cac7b8e63b02166a7cb8f16cd
SolsticeDante <solsticedante@gmail.com>
2019-03-22 10:21:25 +0800

    typo required=true

--- Source/websiteNew/createuser/forms.py
+++ Source/websiteNew/createuser/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class UserForm(forms.Form):
-    username = forms.CharField(label="Username", max_length=100, widget=forms.TextInput, required=true)
-    password = forms.CharField(label="Password", max_length=20, widget=forms.PasswordInput, required=true)
-    email = forms.CharField(label="Email", max_length=50, widget=forms.EmailInput, required=true)
-    phoneNumber = forms.CharField(label="Phone number", max_length=20, widget=forms.NumberInput, required=true)
+    username = forms.CharField(label="Username", max_length=100, widget=forms.TextInput, required=True)
+    password = forms.CharField(label="Password", max_length=20, widget=forms.PasswordInput, required=True)
+    email = forms.CharField(label="Email", max_length=50, widget=forms.EmailInput, required=True)
+    phoneNumber = forms.CharField(label="Phone number", max_length=20, widget=forms.NumberInput, required=True)
 
