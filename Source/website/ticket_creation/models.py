cb07e0e4c2d2aaf1bec6c2a5f4e28d0db4baa5b6
SolsticeDante <solsticedante@gmail.com>
2019-04-07 00:53:12 +0800

    add model for All_Ticket and Ticket_Details

--- Source/website/ticket_creation/models.py
+++ Source/website/ticket_creation/models.py
@@ -8,3 +8,19 @@ class Ticket(models.Model):
     read = models.IntegerField(default=0)
     description = models.CharField(max_length=256)
     user = models.CharField(max_length=60)
+
+class All_Tickets(models.Model):
+    id = models.IntegerField(max_length=20)  # to be determined by total number of tickets
+    size = models.IntegerField(max_length=20)  # for first row, this represents total number of tickets, for other rows it represents number of replies (including ticket itself, starting from 0) for that ticket
+    creator = models.CharField(max_length=20)  # user
+    addressed_by = models.CharField(max_length=20)  # admin
+    resolved_by = models.CharField(max_length=20)  # if None, ticket is not resolved
+    read_by = models.CharField(max_length=100)  # to have names of admins that read the ticket to be concaternated to this value
+    queue_number = models.IntegerField(max_length=20)  # to be implemented in future
+
+class Ticket_Details(models.Model):
+    id = models.IntegerField(max_length=20)
+    title = models.CharField(max_length=60)
+    description = models.CharField(max_length=256)
+    image = models.ImageField(max_legnth=100)  # to be implemented
+    file = models.FileField()  # to be implemented
